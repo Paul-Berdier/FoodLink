b84693fdebd57d99c23d38bf06f4913ac17d2e7e
Paul-Berdier <paul.berdier@ynov.com>
2024-11-27 11:22:15 +0100

    Rename read.me to README.md
    Updated the filename to follow standard naming conventions commonly used for readme files in most repositories. This change improves consistency and clarity for contributors and users.

---
+++ web/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from Login import LoginForm
+from Register import RegisterForm
+
+app = Flask(__name__)
+# app.secret_key = 'your_secret_key_here'
+
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Ajoutez la logique de vérification ici
+        flash('Connexion réussie', 'success')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        # Ajoutez la logique d'inscription ici
+        flash('Inscription réussie', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
